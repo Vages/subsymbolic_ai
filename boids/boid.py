ad41d40fb5b6da0df6f6b75c704f54398a298296
vages <eirikvageskar@gmail.com>
2016-01-21 20:29:00 +0100

    Make hashable (not used yet)

--- boids/boid.py
+++ boids/boid.py
@@ -3,11 +3,23 @@ from math import sin, cos, radians
 
 
 class Boid:
+    id_counter = 0  # Used to give each boid a unique
+
     def __init__(self, pos, vel, world):
         self.position = pos
         self.velocity = vel
         self.angle = radians(random.randrange(0, 360))
         self.world = world
+        self.id = Boid.id_counter
+        Boid.id_counter += 1
+
+    def __eq__(self, other):
+        if self.id == other.id:
+            return True
+        return False
+
+    def __hash__(self):
+        return hash(self.id)
 
     def move(self):
         dx, dy = self.velocity*cos(self.angle), self.velocity*sin(self.angle)
