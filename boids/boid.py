163576a52a5c9b61823783cd25e832036c5c356e
vages <eirikvageskar@gmail.com>
2016-01-21 16:18:11 +0100

    Add simple alignment

--- boids/boid.py
+++ boids/boid.py
@@ -1,12 +1,13 @@
 import random
-from math import sin, cos
+from math import sin, cos, radians
 
 
 class Boid:
-    def __init__(self, pos, vel):
+    def __init__(self, pos, vel, world):
         self.position = pos
         self.velocity = vel
-        self.angle = random.randrange(0, 360)
+        self.angle = radians(random.randrange(0, 360))
+        self.world = world
 
     def move(self):
         dx, dy = self.velocity*cos(self.angle), self.velocity*sin(self.angle)
@@ -14,4 +15,16 @@ class Boid:
         self.position = x+dx, y+dy
 
     def update(self):
+        neighbours = self.world.get_neighbours(self)
+        self.align(neighbours)
         self.move()
+
+    def align(self, neighbours):
+        # Still needs some "latency"
+        s = 0
+        for n in neighbours:
+            s += n.angle
+
+        avg = s/len(neighbours)
+
+        self.angle = avg
--- boids/boid_world.py
+++ boids/boid_world.py
@@ -11,7 +11,7 @@ class BoidWorld:
 
     def add_boid(self):
         x, y = random.randrange(0, self.x_size), random.randrange(0, self.y_size)
-        b = Boid((x, y), 10)
+        b = Boid((x, y), 10, self)
         self.boids.append(b)
 
     def remove_boid(self):
@@ -23,3 +23,7 @@ class BoidWorld:
             boid.update()
             x, y = boid.position
             boid.position = x%self.x_size, y%self.y_size
+
+    def get_neighbours(self, boid):
+        # Must return only within a certain radius
+        return self.boids
