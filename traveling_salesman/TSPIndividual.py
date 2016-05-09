8df2edf99ef356eaec8c4856a463d990490a0feb
Vages <eirikvageskar@gmail.com>
2016-05-09 17:23:53 +0200

    Update classes with hopes of implementing the main loop.

--- traveling_salesman/TSPIndividual.py
+++ traveling_salesman/TSPIndividual.py
@@ -4,16 +4,48 @@ import random
 class TSPIndividual:
     id_counter = 0
 
-    def __init__(self):
+    def __init__(self, genotype=None):
         self.fitnesses = dict()
-        pass
+        self.id = self.id_counter
+        self.id_counter += 1
+
+        if genotype is None:
+            self.genotype = self.generate_random_simple_genotype()
+
+        else:
+            self.genotype = genotype
 
     def dominates(self, other):
-        pass
+        """
+        Returns whether this object dominates the other. Assumes that the goal is to minimize all objectives.
 
+        A solution is said to dominate another if it is no worse than the other in all objectives and superior
+        in at least one. (Lecture slides 9, p. 23)
 
-def generate_random_simple_genotype(length=48):
-    genotype = [i + 1 for i in range(48)]
-    random.shuffle(genotype)
+        :param other: Another TSPIndividual
+        :return:
+        """
+        dominates_in_one_objective = False
+        for k in self.fitnesses:
+            self_score = self.fitnesses[k]
+            other_score = other.fitnesses[k]
 
-    return genotype
+            if self_score < other_score:
+                dominates_in_one_objective = True
+            if self_score > other_score:
+                return False
+
+        return dominates_in_one_objective
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    @staticmethod
+    def generate_random_simple_genotype(length=48):
+        genotype = [i + 1 for i in range(length)]
+        random.shuffle(genotype)
+
+        return genotype
