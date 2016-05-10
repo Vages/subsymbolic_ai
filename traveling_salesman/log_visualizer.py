be5eb0827083101f30af130a87078853ea1c226a
Vages <eirikvageskar@gmail.com>
2016-05-10 19:11:25 +0200

    Make plotting routine even more advanced. Display plots of both pareto front, delegate plotting to function.

--- traveling_salesman/log_visualizer.py
+++ traveling_salesman/log_visualizer.py
@@ -16,15 +16,58 @@ default_file = log_files[-1]
 with open(os.path.join(log_location, default_file), 'r') as log_contents:
     log_data = json.load(log_contents)
 
-x, y = [], []
+plot_counter = 121
 
-for front in log_data:
-    for item in front:
-        dist, cost = item["distance"], item["cost"]
-        x.append(dist)
-        y.append(cost)
+def draw_plot(front_list=log_data, plot_title="All fronts"):
+    global plot_counter
+    x, y = [], []
+    largest_cost = -float("inf")
+    largest_cost_data = ()
+    smallest_cost = float("inf")
+    smallest_cost_data = ()
+    largest_distance = -float("inf")
+    largest_distance_data = ()
+    smallest_distance = float("inf")
+    smallest_distance_data = ()
+    for front in front_list:
+        for item in front:
+            dist, cost = item["distance"], item["cost"]
+            coordinate = (dist, cost)
 
-#area = np.pi * (15 * np.random.rand(N))**2  # 0 to 15 point radiuses
+            if cost < smallest_cost:
+                smallest_cost = cost
+                smallest_cost_data = coordinate
+            if cost > largest_cost:
+                largest_cost = cost
+                largest_cost_data = coordinate
 
-plt.scatter(x, y, alpha=0.5)
+            if dist < smallest_distance:
+                smallest_distance = dist
+                smallest_distance_data = coordinate
+            if dist > largest_distance:
+                largest_distance = dist
+                largest_distance_data = coordinate
+
+            x.append(dist)
+            y.append(cost)
+
+    # area = np.pi * (15 * np.random.rand(N))**2  # 0 to 15 point radiuses
+    plt.subplot(plot_counter)
+    plot_counter += 1
+    plt.scatter(x, y, alpha=0.5)
+    plt.xlabel("distance")
+    plt.ylabel("cost")
+    plt.title(plot_title)
+    plt.text(smallest_distance_data[0], smallest_distance_data[1],
+             "(" + str(smallest_distance_data[0]) + ", " + str(smallest_distance_data[1]) + ")")
+    plt.text(largest_distance_data[0], largest_distance_data[1],
+             "(" + str(largest_distance_data[0]) + ", " + str(largest_distance_data[1]) + ")")
+    plt.text(smallest_cost_data[0], smallest_cost_data[1],
+             "(" + str(smallest_cost_data[0]) + ", " + str(smallest_cost_data[1]) + ")")
+    plt.text(largest_cost_data[0], largest_cost_data[1],
+             "(" + str(largest_cost_data[0]) + ", " + str(largest_cost_data[1]) + ")")
+    plt.grid(True)
+
+draw_plot()
+draw_plot(front_list=[log_data[0]], plot_title="Pareto front")
 plt.show()
